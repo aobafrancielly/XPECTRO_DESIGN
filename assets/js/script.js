526cffb8e342a332d54b315de343f1da0466e7fc
Francielly Silva <143453550+aobafrancielly@users.noreply.github.com>
2026-05-19 15:50:46 -0300

    Delete assets/js directory
    correção

--- assets/js/script.js
+++
@@ -1,5 +0,0 @@
-document.addEventListener('mousemove', (e) => {
-            const glow = document.getElementById('bg-glow');
-            glow.style.setProperty('--mouse-x', `${e.clientX}px`);
-            glow.style.setProperty('--mouse-y', `${e.clientY}px`);
-        });
